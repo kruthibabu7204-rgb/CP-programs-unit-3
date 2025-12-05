8e0ce4d386696d0c71f74eec13e459da3ab119b2
kruthibabu7204-rgb <kruthibabu7204@gmail.com>
2025-12-05 08:17:33 +0530

    Add files via upload

---
+++ 02 program to use typedef with structure.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+typedef struct{
+int id;
+char name[20];
+float marks;
+student;
+}
+int main(){
+student s1={101,"Kiran",97.2};
+printf("student id:%d\n",s1.student id);
+printf("Name:%s\n",s1.name);
+printf("marks:%.2f\n",s1.marks);
+
+return 0;
+}
